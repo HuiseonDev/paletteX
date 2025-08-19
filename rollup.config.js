0975b846019a2f49a1f7d764804f4ec35f925730
전희선 <designh2sun@gmail.com>
2025-08-19 18:05:45 +0900

    Chore: rollup.config.js 파일 수정

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
 
 export default [
-  // React build
+  // React 빌드
   {
     input: "src/index.ts",
     output: [
@@ -12,23 +15,28 @@ export default [
     ],
     plugins: [
       peerDepsExternal(),
-      typescript({
-        tsconfig: "./tsconfig.json",
-        declaration: true,
-        declarationDir: "dist/react",
-      }),
-      postcss({ extract: "dist/button-library.css", minimize: true }),
+      typescript(),
+      postcss({ extract: "dist/button-library.css" }),
+      terser(),
     ],
-    external: ["react", "react-dom"],
   },
-
-  // Vanilla build
+  // TypeScript 선언 파일
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/react/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  },
+  // 바닐라 JS 빌드
   {
     input: "src/vanilla-index.js",
     output: [
       { file: "dist/vanilla/index.esm.js", format: "esm", sourcemap: true },
       { file: "dist/vanilla/index.js", format: "cjs", sourcemap: true },
     ],
-    plugins: [postcss({ extract: "dist/button-library.css", minimize: true })],
+    plugins: [
+      postcss({ extract: "dist/button-library.css" }),
+      terser(),
+      resolve({ extensions: [".js"] }),
+    ],
   },
 ];
